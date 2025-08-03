fdf8fd4c7b0f8e0d3ed5858a7d9e86ccd9a80149
Ruddarm <ruddarmmaurya@gmail.com>
2025-08-03 10:44:28 +0530

    implmented gateway

---
+++ authguard-api-gateway/src/main/java/com/authguard/authguard_api_gateway/AuthguardApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.authguard.authguard_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthguardApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthguardApiGatewayApplication.class, args);
+	}
+
+}
---
+++ authguard-api-gateway/src/test/java/com/authguard/authguard_api_gateway/AuthguardApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authguard.authguard_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthguardApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
